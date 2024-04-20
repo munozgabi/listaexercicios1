af284c691e5a93c0bf9e11e16a8846818dc56cd5
munozgabi <uc10gabriela052@gmail.com>
2024-04-19 21:35:43 -0300

    second commit

--- exe9.js
+++ exe9.js
@@ -2,4 +2,22 @@
 do mesmo, conforme a tabela abaixo */
 
 const prompt = require('prompt-sync')();
-let qtdMacas = parseInt(prompt("Digite a quantidade de maçãs: "));
+let codigo = parseInt(prompt("Digite o código de origem do produto: "));
+
+if(codigo == 1){
+    console.log("Região Sul");
+}else if(codigo == 2){
+    console.log("Região Norte");
+}else if(codigo == 3){
+    console.log("Região Leste");
+}else if(codigo == 4){
+    console.log("Região Oeste");
+}else if(codigo == 5 || codigo == 6 || (codigo > 24 && codigo < 51)){
+    console.log("Região Nordeste");
+}else if(codigo > 6 && codigo < 10){
+    console.log("Região Sudeste");
+}else if(codigo > 9 && codigo < 21 ){
+    console.log("Região Centro-Oeste");
+}else{
+    console.log("Valor inválido");
+}
